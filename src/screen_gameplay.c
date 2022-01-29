587b684e547790454950d2514bcc7649d2735063
Cameron Reikes <cameronreikes@gmail.com>
2022-01-28 20:38:15 -0800

    Rework entity framework and rename extinguisher

--- src/screen_gameplay.c
+++ src/screen_gameplay.c
@@ -32,71 +32,140 @@
 #define SCREEN_SIZE 900 // screen assumed to be square. Used for camera offset
 const float player_radius = 18.0;
 
+typedef struct KinematicInfo
+{
+    Vector2 vel;
+    Vector2 pos;
+    bool onGround;
+} KinematicInfo;
+
+// All the entity datas
+typedef struct PlayerData
+{
+    KinematicInfo k;
+    int grabbedEntity;
+} PlayerData;
+typedef Rectangle ObstacleData;
+typedef Rectangle GroundData;
+typedef struct ExtinguisherData
+{
+    KinematicInfo info;
+    float amountLeft;
+} ExtinguisherData;
+
+// Entity types
 enum Type
 {
+    Player,
     Obstacle,
     Ground,
+    Extinguisher,
 
     // UPDATE MAX_TYPE WHEN YOU CHANGE THIS
 };
-#define MAX_TYPE Ground
+#define MAX_TYPE Extinguisher
 static const char *TypeNames[] = {
+    "Player",
     "Obstacle",
     "Ground",
-};
-
-union Data
-{
-    Rectangle rect;
-    int empty_data[64]; // so I can add new fields without it breaking
+    "Extinguisher",
 };
 
 typedef struct Entity
 {
+    int id;
     enum Type type;
-    union Data data;
+    union
+    {
+        PlayerData player;
+        ObstacleData obstacle;
+        GroundData ground;
+        ExtinguisherData extinguisher;
+        int empty_data[64]; // so I can add new fields without it breaking
+    };
 } Entity;
 
+// editor state
 static bool editing = true;
-static Entity currentEntity;
-static bool creatingEntity = false;
-static enum Type currentType = Obstacle;
+static int currentType = 0;
+static Entity *currentEntity = NULL;
 
+// game state
 static int finishScreen = 0;
-static Vector2 position = {.x = 200, .y = 300};
 static Camera2D camera;
-static Vector2 velocity = {0};
-static Vector2 fireExtinguisherPosition = {100, 100};
-static Texture fireExtinguisher;
 
+// entity stuff
+typedef int ID;
 static Entity entities[100];
-static size_t entities_len = 0;
-//----------------------------------------------------------------------------------
-// Gameplay Screen Functions Definition
-//----------------------------------------------------------------------------------
+static size_t entitiesLen = 0;
+static ID curNextEntityID = 0;
 
-// returns whichever has greater magnitude
-float absmax(float a, float b)
+int GetEntityIndex(ID id)
 {
-    if (fabs(a) > fabs(b))
+    for (int i = 0; i < entitiesLen; i++)
     {
-        return a;
+        if (entities[i].id == id)
+        {
+            return i;
+        }
     }
-    return b;
+    return -1;
 }
 
-void RemoveEntity(int index)
+// null if entity doesn't exist
+Entity *GetEntity(ID id)
 {
-    for (int i = index; i < entities_len - 1; i++)
+    int index = GetEntityIndex(id);
+    if (index == -1)
+    {
+        return NULL;
+    }
+    return &entities[index];
+}
+
+// null if player not in entities
+Entity *GetPlayerEntity()
+{
+    for (int i = 0; i < entitiesLen; i++)
+    {
+        if (entities[i].type == Player)
+        {
+            return &entities[i];
+        }
+    }
+    return NULL;
+}
+
+void DeleteEntityIndex(int index)
+{
+    for (int i = index; i < entitiesLen - 1; i++)
     {
         entities[i] = entities[i + 1];
     }
-    entities_len--;
+    entitiesLen -= 1;
+}
+
+void DeleteEntity(ID id)
+{
+    int index = GetEntityIndex(id);
+    if (index != -1)
+    {
+        DeleteEntityIndex(index);
+    }
+}
+
+Entity *AddEntity(Entity e)
+{
+    e.id = curNextEntityID;
+    curNextEntityID += 1;
+    entities[entitiesLen] = e;
+    entitiesLen += 1;
+    return &entities[entitiesLen - 1];
 }
 
 void SaveEntities(const char *path)
 {
-    SaveFileData(path, (void *)entities, entities_len * sizeof(Entity));
+    SaveFileData(path, (void *)entities, entitiesLen * sizeof(Entity));
 }
 void LoadEntities(const char *path)
 {
@@ -105,8 +174,10 @@ void LoadEntities(const char *path)
     for (int i = 0; i < bytesRead / sizeof(Entity); i++)
     {
         entities[i] = ((Entity *)data)[i];
+        curNextEntityID = max(curNextEntityID, entities[i].id);
     }
-    entities_len = bytesRead / sizeof(Entity);
+    curNextEntityID = curNextEntityID + 1;
+    entitiesLen = bytesRead / sizeof(Entity);
     UnloadFileText(data);
 
     // for debugging stuff, also put any programmatic level modifications here
@@ -119,6 +190,28 @@ void LoadEntities(const char *path)
         }
         printf("%f %f %f %f\n", r.x, r.y, r.width, r.height);
     }*/
+}
+
+int max(int a, int b)
+{
+    if (a > b)
+        return a;
+    return b;
+}
+
+// returns whichever has greater magnitude
+float absmax(float a, float b)
+{
+    if (fabs(a) > fabs(b))
+    {
+        return a;
+    }
+    return b;
+}
+
+void DrawTexCentered(Texture t, Vector2 pos, float scale)
+{
+    DrawTextureEx(t, Vector2Add(pos, Vector2Scale((Vector2){.x = t.width, .y = t.height}, -scale * 0.5)), 0.0f, scale, WHITE);
 }
 
 float clamp(float value, float min, float max)
@@ -145,12 +238,22 @@ Vector2 Vector2Project(Vector2 a, Vector2 b)
     return Vector2Scale(Vector2Normalize(b), Vector2DotProduct(a, b) / Vector2Length(b));
 }
 
+enum Textures
+{
+    EXTINGUISHER_TEXTURE,
+};
+
+static Texture textures[] = {
+    {0},
+};
+
 // Gameplay Screen Initialization logic
 void InitGameplayScreen(void)
 {
+    textures[EXTINGUISHER_TEXTURE] = LoadTexture("resources/Extinguisher.png");
     camera = (Camera2D){
         .offset = (Vector2){.x = SCREEN_SIZE / 2.0f, .y = SCREEN_SIZE / 2.0f},
-        .target = position,
+        .target = (Vector2){0},
         .rotation = 0.0,
         .zoom = 1.0,
     };
@@ -161,27 +264,26 @@ void InitGameplayScreen(void)
     else
     {
         entities[0] = (Entity){
-            .type = Obstacle,
-            .data = (Rectangle){
-                .x = 400,
-                .y = 400,
-                .width = 100,
-                .height = 200,
+            .type = Player,
+            .player = {
+                .k = {
+                    .pos = (Vector2){200, 300},
+                    .vel = (Vector2){0},
+                },
+                .grabbedEntity = -1,
             },
         };
         entities[1] = (Entity){
             .type = Obstacle,
-            .data = (Rectangle){
+            .obstacle = {
                 .x = 100,
                 .y = 400,
                 .width = 100,
                 .height = 200,
             },
         };
-        entities_len = 2;
+        entitiesLen = 2;
     }
-
-    fireExtinguisher = LoadTexture("resources/Fire Extinguisher.png");
 }
 
 Rectangle FixNegativeRect(Rectangle rect)
@@ -206,47 +308,91 @@ bool RectHasPoint(Rectangle rect, Vector2 point)
     return CheckCollisionPointRec(point, rect);
 }
 
-void ProcessEntity(Entity *entities, size_t i)
+KinematicInfo GlideAndBounce(KinematicInfo k)
 {
-    switch (entities[i].type)
+    k.onGround = false;
+    for (int i = 0; i < entitiesLen; i++)
     {
-    case Obstacle:
-    {
-        Rectangle obstacle = FixNegativeRect(entities[i].data.rect);
-        Vector2 normal = {0};
-        Vector2 newPosition = position;
+        if (entities[i].type == Obstacle)
         {
-            Vector2 obstacleCenter = {.x = obstacle.x + obstacle.width / 2.0f, .y = obstacle.y + obstacle.height / 2.0f};
-            Vector2 fromObstacleCenter = Vector2Subtract(position, obstacleCenter);
-            fromObstacleCenter.x = clamp(fromObstacleCenter.x, -obstacle.width / 2.0f, obstacle.width / 2.0f);
-            fromObstacleCenter.y = clamp(fromObstacleCenter.y, -obstacle.height / 2.0f, obstacle.height / 2.0f);
-            Vector2 closestPointOnObstacle = Vector2Add(fromObstacleCenter, obstacleCenter);
-            if (Vector2Distance(closestPointOnObstacle, position) < player_radius)
+            Rectangle obstacle = FixNegativeRect(entities[i].obstacle);
+            Vector2 normal = {0};
             {
-                normal = Vector2Normalize(Vector2Subtract(position, closestPointOnObstacle));
-                newPosition = Vector2Add(closestPointOnObstacle, Vector2Scale(normal, player_radius));
+                Vector2 obstacleCenter = {.x = obstacle.x + obstacle.width / 2.0f, .y = obstacle.y + obstacle.height / 2.0f};
+                Vector2 fromObstacleCenter = Vector2Subtract(k.pos, obstacleCenter);
+                fromObstacleCenter.x = clamp(fromObstacleCenter.x, -obstacle.width / 2.0f, obstacle.width / 2.0f);
+                fromObstacleCenter.y = clamp(fromObstacleCenter.y, -obstacle.height / 2.0f, obstacle.height / 2.0f);
+                Vector2 closestPointOnObstacle = Vector2Add(fromObstacleCenter, obstacleCenter);
+                if (Vector2Distance(closestPointOnObstacle, k.pos) < player_radius)
+                {
+                    normal = Vector2Normalize(Vector2Subtract(k.pos, closestPointOnObstacle));
+                    k.pos = Vector2Add(closestPointOnObstacle, Vector2Scale(normal, player_radius));
+                }
             }
+            k.vel = Vector2Reflect(k.vel, normal);
         }
-        position = newPosition;
-        velocity = Vector2Reflect(velocity, normal);
+        else if (entities[i].type == Ground && RectHasPoint(entities[i].obstacle, k.pos))
+        {
+            k.onGround = true;
+        }
+    }
+    return k;
+}
+
+void ProcessEntity(Entity *e)
+{
+    switch (e->type)
+    {
+    case Player:
+    {
+        camera.target = Vector2Lerp(camera.target, e->player.k.pos, GetFrameTime() * 5.0);
+        float delta = GetFrameTime();
+        Vector2 movement = {
+            .x = (float)IsKeyDown(KEY_D) - (float)IsKeyDown(KEY_A),
+            .y = (float)IsKeyDown(KEY_S) - (float)IsKeyDown(KEY_W),
+        };
+
+        movement = Vector2Normalize(movement);
+        e->player.k = GlideAndBounce(e->player.k);
+        if (e->player.k.onGround)
+            e->player.k.vel = Vector2Lerp(e->player.k.vel, Vector2Scale(movement, 50.0f), delta * 4.0f);
+        e->player.k.pos = Vector2Add(e->player.k.pos, Vector2Scale(e->player.k.vel, delta));
         break;
+    }
+    case Extinguisher:
+    {
+        if (GetPlayerEntity()->player.grabbedEntity != e->id)
+        {
+            e->extinguisher.info = GlideAndBounce(e->extinguisher.info);
+            break;
+        }
     }
     }
 }
 
-void DrawEntity(Entity *entities, size_t i)
+void DrawEntity(Entity e)
 {
-    Entity e = entities[i];
     switch (e.type)
     {
+    case Player:
+    {
+        DrawCircleV(e.player.k.pos, player_radius, PINK);
+        break;
+    }
     case Obstacle:
     {
-        DrawRectanglePro(FixNegativeRect(e.data.rect), (Vector2){0}, 0.0, GRAY);
+        DrawRectanglePro(FixNegativeRect(e.obstacle), (Vector2){0}, 0.0, GRAY);
         break;
     }
     case Ground:
     {
-        DrawRectanglePro(FixNegativeRect(e.data.rect), (Vector2){0}, 0.0, GREEN);
+        printf("Drawing %f %f\n", e.ground.x, e.ground.width);
+        DrawRectanglePro(FixNegativeRect(e.ground), (Vector2){0}, 0.0, GREEN);
+        break;
+    }
+    case Extinguisher:
+    {
+        DrawTexCentered(textures[EXTINGUISHER_TEXTURE], e.extinguisher.info.pos, 0.35f);
         break;
     }
     }
@@ -257,25 +403,10 @@ void UpdateGameplayScreen(void)
     if (IsKeyPressed(KEY_TAB))
         editing = !editing;
 
-    camera.target = Vector2Lerp(camera.target, position, GetFrameTime() * 5.0);
-    float delta = GetFrameTime();
-    Vector2 movement = {
-        .x = (float)IsKeyDown(KEY_D) - (float)IsKeyDown(KEY_A),
-        .y = (float)IsKeyDown(KEY_S) - (float)IsKeyDown(KEY_W),
-    };
-
-    movement = Vector2Normalize(movement);
-
-    bool onGround = false;
-    for (int i = 0; i < entities_len; i++)
+    for (int i = 0; i < entitiesLen; i++)
     {
-        if (entities[i].type == Ground && RectHasPoint(entities[i].data.rect, position))
-            onGround = true;
         ProcessEntity(entities, i);
     }
-    if (onGround)
-        velocity = Vector2Lerp(velocity, Vector2Scale(movement, 400.0f), delta * 9.0f);
-    position = Vector2Add(position, Vector2Scale(velocity, delta));
 
     // separate loops for gameplay and editing so editing can break early (deleting
     // multiple entities per loop I can't be bothered to implement)
@@ -292,39 +423,68 @@ void UpdateGameplayScreen(void)
             LoadEntities("resources/saved.level");
         }
         if (IsMouseButtonDown(MOUSE_BUTTON_MIDDLE))
-            position = WorldMousePos();
+            GetPlayerEntity()->player.k.pos = WorldMousePos();
         if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT))
         {
-            creatingEntity = true;
-            currentEntity.type = currentType;
-            currentEntity.data.rect.x = WorldMousePos().x;
-            currentEntity.data.rect.y = WorldMousePos().y;
-        }
-        currentEntity.data.rect.width = WorldMousePos().x - currentEntity.data.rect.x;
-        currentEntity.data.rect.height = WorldMousePos().y - currentEntity.data.rect.y;
-        currentEntity.data.rect.width = absmax(3.0, currentEntity.data.rect.width);
-        currentEntity.data.rect.height = absmax(3.0, currentEntity.data.rect.height);
-        if (IsMouseButtonReleased(MOUSE_BUTTON_LEFT))
-        {
-            entities[entities_len] = currentEntity;
-            entities_len += 1;
-            creatingEntity = false;
+            if (currentType == Player)
+            {
+            }
+            else
+            {
+                Entity toAdd = {0};
+                toAdd.type = currentType;
+                if (currentType == Ground || currentType == Obstacle)
+                {
+                    toAdd.ground.x = WorldMousePos().x;
+                    toAdd.ground.y = WorldMousePos().y;
+                }
+                else
+                {
+                    toAdd.extinguisher.info.pos = WorldMousePos();
+                    toAdd.extinguisher.info.vel = (Vector2){0};
+                }
+                currentEntity = AddEntity(toAdd);
+            }
         }
 
-        for (int i = 0; i < entities_len; i++)
+        if (currentEntity != NULL && (currentEntity->type == Ground || currentEntity->type == Obstacle))
         {
-            switch (entities[i].type)
+            currentEntity->ground.width = WorldMousePos().x - currentEntity->ground.x;
+            currentEntity->ground.height = WorldMousePos().y - currentEntity->ground.y;
+            currentEntity->ground.width = absmax(3.0, currentEntity->ground.width);
+            currentEntity->ground.height = absmax(3.0, currentEntity->ground.height);
+        }
+        if (IsMouseButtonReleased(MOUSE_BUTTON_LEFT))
+        {
+            currentEntity = NULL;
+        }
+
+        if (IsMouseButtonDown(MOUSE_BUTTON_RIGHT))
+        {
+            for (int i = 0; i < entitiesLen; i++)
             {
-            case Obstacle:
-            case Ground:
-            {
-                if (IsMouseButtonDown(MOUSE_BUTTON_RIGHT) && RectHasPoint(entities[i].data.rect, WorldMousePos()))
+                switch (entities[i].type)
                 {
-                    RemoveEntity(i);
+                case Obstacle:
+                case Ground:
+                {
+                    if (RectHasPoint(entities[i].ground, WorldMousePos()))
+                    {
+                        DeleteEntityIndex(i);
+                        break;
+                    }
                     break;
                 }
-                break;
-            }
+                case Extinguisher:
+                {
+                    if (Vector2Distance(entities[i].extinguisher.info.pos, WorldMousePos()) < 15.0f)
+                    {
+                        DeleteEntityIndex(i);
+                        break;
+                    }
+                    break;
+                }
+                }
             }
         }
     }
@@ -335,17 +495,13 @@ void DrawGameplayScreen(void)
 {
     DrawRectangle(0, 0, GetScreenWidth(), GetScreenHeight(), (Color){17, 17, 17, 255});
     BeginMode2D(camera);
-
-    for (int i = 0; i < entities_len; i++)
+    for (int i = 0; i < entitiesLen; i++)
     {
-        DrawEntity(entities, i);
+        if(entities[i].type == Player)
+            continue;
+        DrawEntity(entities[i]);
     }
-    if (editing && creatingEntity)
-    {
-        DrawEntity(&currentEntity, 0);
-    }
-    DrawCircleV(position, player_radius, PINK);
-    DrawTextureEx(fireExtinguisher, fireExtinguisherPosition, 0.0f, 0.35f, WHITE);
+    DrawEntity(*GetPlayerEntity());
     EndMode2D();
 
     if (editing)
